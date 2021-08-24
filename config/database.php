2074aebcf8ecef9e6b53a68847309e05c9e19986
creed1120 <cedricareed@gmail.com>
2021-08-24 16:02:15 -0700

    Update database connection

--- config/database.php
+++ config/database.php
@@ -1,4 +1,8 @@
+
+
 <?php
+
+$DATABASE_URL=parse_url('postgres://abzynlpfibnnua:f15cef22cd38efd7f06b6fe0c5f59642066d2f2f5939a15f0417517a8e0a638c@ec2-54-156-60-12.compute-1.amazonaws.com:5432/d6en9etb9krnuc');
 
 use Illuminate\Support\Str;
 
@@ -15,7 +19,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +69,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            // 'url' => env('DATABASE_URL'),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
