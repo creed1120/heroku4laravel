61132e19123a98d579f09b5d728a2f51bb9fad46
creed1120 <cedricareed@gmail.com>
2021-08-24 17:19:54 -0700

    added new feature

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,7 +39,7 @@
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                     <h1>Master Branch Fool!</h1>
-                    <h3>New entry merged from Dev Branch</h3>
+                    <h3>THE NEW FEATURE</h3>
                 </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
